0ef73bb65dd05d3e877e5d0a6d215c6fe62f5e80
LuK1337 <priv.luk@gmail.com>
2023-10-26 18:21:55 +0200

    Declare maven publication
    Change-Id: Ie084a6706e093c72854fc455816fd752db005574

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+group = "com.hp"
+version = "1.0"
+
 plugins {
     `java`
+    `maven-publish`
 }
 
 java {
@@ -9,4 +13,39 @@ java {
 
 sourceSets {
     getByName("main").java.srcDirs("src")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            pom {
+                name.set(rootProject.name)
+                url.set("https://github.com/lineage-next/crcalc")
+
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("The Android Open Source Project")
+                        url.set("https://android.com")
+                    }
+
+                    developer {
+                        name.set("Silicon Graphics, Inc.")
+                        url.set("https://sgi.com")
+                    }
+
+                    developer {
+                        name.set("Hewlett-Packard Company")
+                        url.set("https://hp.com")
+                    }
+                }
+            }
+        }
+    }
 }
